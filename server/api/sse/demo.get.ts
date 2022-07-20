cde4d5a125c31fde21093ad1d75454f1bba7c68d
Hal Heisler <hal@sunteksolutions.onmicrosoft.com>
2022-07-19 21:33:53 -0700

    api/sse/demo returns text/event-stream

---
+++ server/api/sse/demo.get.ts
@@ -0,0 +1,32 @@
+import crypto from 'crypto';
+
+export default defineEventHandler(async (e) => {
+    //Returning headers to the client
+    e.res.writeHead(200, {
+        'Cache-Control': 'no-cache',
+        Connection: 'keep-alive',
+        'Content-Type': 'text/event-stream',
+    });
+
+    const sseId = crypto.randomUUID();
+
+    const sendEvent = (data: string) => {
+        e.res.write(`id: ${sseId}\n`);
+        e.res.write(`data: ${data}\n\n`);
+      };
+
+    const sleep = (milliseconds) => {
+        return new Promise((resolve) => setTimeout(resolve, milliseconds));
+    };
+
+    let times = 10;
+
+    for (let i = 0; i < times; i++) {
+        const data = {date:new Date(), eventCount:i};
+        sendEvent(JSON.stringify(data));
+        await sleep(1000);
+    }
+
+    sendEvent('[DONE]');
+    e.res.end();
+});
